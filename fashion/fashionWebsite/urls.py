4d0bef42c900e8261ec705bd5a5615d60fc9464b
JessesCUkaegbu <ukaegbujesses@gmail.com>
2024-03-21 09:30:18 +0000

    another commit is made

--- fashion/fashionWebsite/urls.py
+++ fashion/fashionWebsite/urls.py
@@ -4,11 +4,10 @@ from . import views
 
 
 
-
 urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about, name="about"),
     path('service/', views.service, name="service"),
-    path('blog', views.blog, name="blog"),
-    path('contact', views.contact, name="contact"),
+    path('blog/', views.blog, name="blog"),
+    path('contact/', views.contact, name="contact"),
 ]
